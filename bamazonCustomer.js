f4c00c519ca324d62e677b1303978cde5c6b7e40
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-11-01 19:08:12 -0700

    Use colors pkg to display products in-stock.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,9 @@
 // Require MySQL and Inquirer
 const mysql = require('mysql')
 const inquirer = require('inquirer');
+const colors = require('colors');
 
 // Require custom modules
-// @todo import methids instead of single function. Create methods for quantity and purchase
 let {purchase, quantity} = require('./custom_modules/purchase');
 
 // Make a connection to the database
@@ -22,19 +22,26 @@ dbConnection.query('SELECT * FROM products', function (error, results) {
 	}
 	//Initialize the products array this will be passed Inquirer to validate
 	var productsArray = results;
-
-	productsArray.map(item => {
-		console.log(`Item ID: ${item.item_id} | Name: ${item.product_name} | Price: $${item.price}`);
+	productsArray.map((item, index) => {
+		// Alternate the colors for each product as they are displayed to the terminal
+		var color = index % 2 === 0 ? "red" : "magenta";
+		console.log(`Item ID: ${item.item_id} | Name: ${item.product_name} | Price: $${item.price}`[color]);
 	});
+
+	// Begin to ask the customer which product they would like to purchase and how many.	
 	inquirer.prompt([{
 		type: 'input',
 		name: 'item_to_purchase',
 		message: 'Enter the ID of the product you would like to purchase:',
+		// Use the purchase function created by the purchase module to validate the customer's choice:
+		// 1) Entry is a number
+		// 2) Confirm the number is matches the ID of a product in the database
 		validate: purchase
 	}, {
 		type: 'input',
 		name: 'item_quantity',
 		message: 'How many would you would like to purchase?',
+		// Use the quantity function created by the purchase module to make sure there is enough of the product in stock to fulfill the customer's order.
 		validate: quantity
 	}]).then(function inqAnswers(answers) {
 		console.log(answers);
--- custom_modules/purchase.js
+++ custom_modules/purchase.js
@@ -1,19 +1,22 @@
+const colors = require('colors');
+
 function purchase (item_id) {
 	let item = new Number(item_id);
 
 	if (isNaN(item)){
-		console.log("\nItem is a NOT number. Please enter a valid ID number");
+		console.log("\n\nItem is a NOT number. Please enter a valid ID number\n".red);	
 		return false;
 	}
 	
 	else {
-		console.log("\nItem is a number.");
+		// @todo entry must be a valid ID.
 		return true;
 	}
 }
 
 function quantity(num_to_purchase) {
 	console.log(`\nNumber of items to purchase: ${num_to_purchase}`);
+	return true;
 }
 
 module.exports = {
