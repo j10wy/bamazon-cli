b888dc6a7477fe93681511ad9cea16a89cc22d35
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-11-03 00:15:49 -0700

    Item ID validation

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,7 +4,10 @@ const inquirer = require('inquirer');
 const colors = require('colors');
 
 // Require custom modules
-let {purchase, quantity} = require('./custom_modules/purchase');
+let {
+	Purchase,
+	Quantity
+} = require('./custom_modules/purchase');
 
 // Make a connection to the database
 const dbConnection = mysql.createConnection({
@@ -14,6 +17,8 @@ const dbConnection = mysql.createConnection({
 	database: 'bamazon'
 });
 
+var productsArray = [];
+
 // Create the inital query to the products table
 dbConnection.query('SELECT * FROM products', function (error, results) {
 	// Throw an error if the database cannot be queried. 
@@ -21,11 +26,11 @@ dbConnection.query('SELECT * FROM products', function (error, results) {
 		throw error;
 	}
 	//Initialize the products array this will be passed Inquirer to validate
-	var productsArray = results;
+	productsArray = results;
 	productsArray.map((item, index) => {
 		// Alternate the colors for each product as they are displayed to the terminal
 		var color = index % 2 === 0 ? "red" : "magenta";
-		console.log(`Item ID: ${item.item_id} | Name: ${item.product_name} | Price: $${item.price}`[color]);
+		console.log(`Item ID: ${item.item_id} | Name: ${item.product_name} | Price: $${item.price}` [color]);
 	});
 
 	// Begin to ask the customer which product they would like to purchase and how many.	
@@ -33,16 +38,20 @@ dbConnection.query('SELECT * FROM products', function (error, results) {
 		type: 'input',
 		name: 'item_to_purchase',
 		message: 'Enter the ID of the product you would like to purchase:',
-		// Use the purchase function created by the purchase module to validate the customer's choice:
+		// Use the Purchase constructor created by the purchase module to validate the customer's choice:
 		// 1) Entry is a number
 		// 2) Confirm the number is matches the ID of a product in the database
-		validate: purchase
+		validate: (value) => {
+			return new Purchase(value, productsArray).validate();
+		}
 	}, {
 		type: 'input',
 		name: 'item_quantity',
 		message: 'How many would you would like to purchase?',
-		// Use the quantity function created by the purchase module to make sure there is enough of the product in stock to fulfill the customer's order.
-		validate: quantity
+		// Use the Quantity constructor created by the purchase module to make sure there is enough of the product in stock to fulfill the customer's order.
+		validate: (value) => {
+			return new Quantity(value, productsArray).validate();
+		}
 	}]).then(function inqAnswers(answers) {
 		console.log(answers);
 		dbConnection.end();
--- custom_modules/purchase.js
+++ custom_modules/purchase.js
@@ -1,25 +1,39 @@
 const colors = require('colors');
 
-function purchase (item_id) {
-	let item = new Number(item_id);
+function Purchase(item_id, productsArray) {
+	this.item = parseInt(item_id);
+	this.productsArray = productsArray;
+}
 
-	if (isNaN(item)){
-		console.log("\n\nItem is a NOT number. Please enter a valid ID number\n".red);	
+// @todo Comment code for validate method.
+Purchase.prototype.validate = function validate() {
+	if (isNaN(this.item)) {
+		console.log("\n\nItem is a NOT number. Please enter a valid ID number\n".red);
 		return false;
-	}
-	
-	else {
-		// @todo entry must be a valid ID.
-		return true;
+	} else {
+		var valid_product_id = false;
+		this.productsArray.map((item, index) => {
+			if (this.item === item.item_id) {
+				valid_product_id = true;
+			}
+		});
+		if (valid_product_id) {
+			return true;
+		} else {
+			console.log("\n\nItem is a valid ID number. Please try again.\n".red);
+			return false;
+		}
 	}
 }
 
-function quantity(num_to_purchase) {
+// @todo finish Quantity Constructor.
+function Quantity(num_to_purchase) {
 	console.log(`\nNumber of items to purchase: ${num_to_purchase}`);
 	return true;
 }
 
+
 module.exports = {
-	purchase: purchase,
-	quantity:quantity
+	Purchase: Purchase,
+	Quantity: Quantity
 }
